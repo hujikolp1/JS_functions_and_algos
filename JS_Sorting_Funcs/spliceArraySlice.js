a18041ad7d00af7c85ebb99fcd72d072adb8f26e
unknown <harold9188@gmail.com>
2021-02-14 14:29:53 -0800

    splice rest params

--- JS_Sorting_Funcs/spliceArraySlice.js
+++ JS_Sorting_Funcs/spliceArraySlice.js
@@ -12,4 +12,13 @@ console.log("sliceVal before ", sliceVal);
 sliceVal.splice(0,spliceVal.length/2);
 console.log("sliceVal after ", sliceVal);
 
+const numArr = [100, 101, 102, 103, 105];
+console.log("before -> ", numArr)
+let startIndex = 2;
+let amountToDelete = 1; 
+let valuesToAdd = [106,107];
+numArr.splice(startIndex, amountToDelete, ...valuesToAdd);
+console.log("after -> ", numArr)
+
+
 
