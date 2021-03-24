16c710995531c8224f29405ba5b9f102ce2f1d9f
unknown <harold9188@gmail.com>
2021-03-24 15:23:28 -0700

    unique shadow finds uniques in arrays

---
+++ JS_Sorting_Funcs/uniqueShadow.js
@@ -0,0 +1,18 @@
+function uniteUnique(arr) {
+    let argsArr = Array.from(arguments)
+    let shadow = []
+    let res=[]; 
+    for(let i in argsArr){
+      for(let k in argsArr[i]){
+        if( shadow[argsArr[i][k]] === undefined){
+          res.push( argsArr[i][k] )
+          shadow[argsArr[i][k]] = 1
+        }
+      }
+    }
+    
+    console.log('-> ', res)
+    return res;
+  }
+  
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) 
