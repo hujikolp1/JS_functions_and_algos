7367b9f626a75a27232a6bbea2c325decc58bbdd
bhu <harold9188@gmail.com>
2021-06-08 11:03:04 -0700

    output equallySpaced

--- JS_Sorting_Funcs/equallySpaced.js
+++ JS_Sorting_Funcs/equallySpaced.js
@@ -1,10 +1,11 @@
 // Algorithm to equally space a variable number of objects onto a line
 
-// --- VARS ---
-// 1 == line length 
-// 2 == object length 
-// 3 == equal part lengths 
-// 4 == plus minus half size of object
+// --- Vars ---
+//
+// 1) line length 
+// 2) object length 
+// 3) equal part lengths 
+// 4) plus minus half size of object
 
 var lineLength; 
 var objectsNumber; 
@@ -12,3 +13,7 @@ var objectsLength;
 var equalParts = lineLength / objectsNumber; 
 var halfSize = objectsNumber / 2; 
 
+// --- Output ---
+// 
+// points on line equal part lengths
+// plus and minus halfSize onto each point  
